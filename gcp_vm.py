a4a24f68ba1a6a2ca44523152dd9e9131ba25195
aadesh <aadeshkale0@gmail.com>
2020-09-04 20:52:42 +0530

    get all zones in gcp method complated

--- gcp_vm.py
+++ gcp_vm.py
@@ -1,28 +1,35 @@
 """
 This script perform some checks on GCP compute engine VM's
 """
-import os
-import json
 import argparse
 import sys
 import csv
+from googleapiclient import discovery
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file('credentials/my_credentials.json')
+COMPUTE_CLIENT = discovery.build('compute', 'v1', credentials=credentials)
+PROJECT_ID = "info1-284008"
 
 
+class Resource:
+
+    def __init__(self,compute_client,project):
+        self.compute_client = compute_client
+        self.project = project
+
+    def get_zones(self):
+        zones = self.compute_client.zones().list(project=self.project).execute()
+        zones_list = []
+        for i in zones['items']:
+            zones_list.append(i['name'])
+        return zones_list
+
+    # def list_instances(self, compute, project, zone):
+    #     result = compute.instances().list(project=project, zone=zone).execute()
+    #     return result['items'] if 'items' in result else None
 
 
-
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--service_account",
-                        help="Pass service account file path with appropriate permissions", type=str)
-    args = parser.parse_args()
-    try:
-        path = args.service_account
-    except Exception as e:
-        print("Exception:", e)
-        sys.exit()
-
-
+res = Resource(compute_client=COMPUTE_CLIENT,project=PROJECT_ID)
+print(res.get_zones())
 
