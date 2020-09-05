b3f0a7277419f6997d281c35395aabb4d1c64d02
aadesh <aadeshkale0@gmail.com>
2020-09-05 12:57:13 +0530

    vm check complate which are not running

--- gcp_vm.py
+++ gcp_vm.py
@@ -1,28 +1,81 @@
 """
 This script perform some checks on GCP compute engine VM's
 """
-import argparse
-import sys
+import os
 import csv
 from googleapiclient import discovery
 from google.oauth2 import service_account
 
-credentials = service_account.Credentials.from_service_account_file('credentials/my_credentials.json')
-COMPUTE_CLIENT = discovery.build('compute', 'v1', credentials=credentials)
 PROJECT_ID = "info1-284008"
+SERVICE_ACCOUNT_FILE_PATH = "credentials/my_credentials.json"
+
+# client api for communication
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE_PATH)
+COMPUTE_CLIENT = discovery.build('compute', 'v1', credentials=credentials)
 
 
-# this class perform different checks on all gcp compute engines
 class Checks:
+    """
+        this class perform different checks on all gcp compute engines
+    """
     def __init__(self, all_info):
         self.all_info = all_info
 
+    # --- check methods ---
+    # this method check compute engine instances which are not running
+    def check_1_1_instances_which_are_not_running(self):
+        check_id = 1.1
+        description = "Check for compute engine instances which are not running"
+        if len(self.all_info) <= 0:
+            self.result_template(
+                check_id=check_id,
+                result=False,
+                reason="There is no gcp compute engine instances",
+                resource_list=[],
+                description=description
+            )
+        else:
+            resource_list = []
+            for reg, inst in self.all_info.items():
+                for m in inst:
+                    if m['status'] != 'RUNNING':
+                        resource_list.append(m['id'])
 
+            if len(resource_list) > 0:
+                result = True
+                reason = "Compute engine instances are not running"
+            else:
+                result = False
+                reason = "All Compute engine instances are running"
+            return self.result_template(check_id, result, reason, resource_list, description)
+
+    # --- supporting methods ---
+    # this method generates template for each check
+    def result_template(self, check_id, result, reason, resource_list, description):
+        template = dict()
+        template['check_id'] = check_id
+        template['result'] = result
+        template['reason'] = reason
+        template['resource_list'] = resource_list
+        template['description'] = description
+        return template
+
+    # this method generate csv file for check results
+    def generate_csv(self, all_check_result):
+        with open('gcp_vm.csv', 'w') as outcsv:
+            headers = ["check_id", "result", "reason", "resource_list", "description"]
+            writer = csv.DictWriter(outcsv, fieldnames=headers)
+            writer.writeheader()
+            for row in all_check_result:
+                writer.writerow(row)
+        print("Output write to:gcp_vm.csv")
 
 
 class Resource:
-
-    def __init__(self,compute_client,project):
+    """
+        this class get different resource information to perform checks on all gcp compute engines
+    """
+    def __init__(self, compute_client, project):
         self.compute_client = compute_client
         self.project = project
 
@@ -45,6 +98,22 @@ class Resource:
         return all_info
 
 
-res = Resource(compute_client=COMPUTE_CLIENT,project=PROJECT_ID)
-print(res.all_instances())
+class ExecuteCheck:
+    """
+        This class Execute all check and generates report
+    """
+    # this method execute checks
+    def perform_check(self):
+        # getting resources for performing check
+        resource_obj = Resource(compute_client=COMPUTE_CLIENT, project=PROJECT_ID)
+        all_info = resource_obj.all_instances()
+        #
+        check_obj = Checks(all_info=all_info)
+        all_check_result = [
+            check_obj.check_1_1_instances_which_are_not_running(),
+        ]
+        check_obj.generate_csv(all_check_result)
 
+
+exp = ExecuteCheck()
+exp.perform_check()
