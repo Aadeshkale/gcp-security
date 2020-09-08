a8a8ec68cd1d21475608e7a60011e2aebe930d05
aadesh <aadeshkale0@gmail.com>
2020-09-09 00:33:37 +0530

    iam checks are added

--- gcp_iam.py
+++ gcp_iam.py
@@ -13,9 +13,10 @@ class IamChecks:
     """
         this class perform different checks on GCP Iam
     """
-    def __init__(self, iam_client, all_service_accounts):
+    def __init__(self, iam_client, all_service_accounts, policies):
         self.iam_client = iam_client
         self.all_service_accounts = all_service_accounts
+        self.policies = policies
 
     # --- check methods ---
     # this method check service account does have user managed key
@@ -73,9 +74,156 @@ class IamChecks:
                 reason = "There is no service account which is disabled"
             return self.result_template(check_id, result, reason, resource_list, description)
 
-    # this method service account has project owner permission
+    # this method service account has project owner policy role
+    def check_3_3_service_account_has_owner_permission(self):
+        check_id = 3.3
+        description = "Check for gcp service account has project owner policy role"
+        if len(self.all_service_accounts) <= 0:
+            self.result_template(
+                check_id=check_id,
+                result=False,
+                reason="There is no gcp iam service account created",
+                resource_list=[],
+                description=description
+            )
+        else:
+            resource_list = []
+            for sacc in self.all_service_accounts:
+                if len(self.policies) > 0:
+                    for p in self.policies:
+                        if sacc['name'] in p['members'] and p['role'] == 'roles/owner':
+                            resource_list.append(sacc['name'])
 
+            if len(resource_list) > 0:
+                result = True
+                reason = "Gcp service account has project owner policy role"
+            else:
+                result = False
+                reason = "There is no service account with project owner policy role"
+            return self.result_template(check_id, result, reason, resource_list, description)
 
+    # this method check gcp project has more than one owner user
+    def check_3_4_more_than_one_owner(self):
+        check_id = 3.4
+        description = "Check for gcp project has more than one owner user"
+        if len(self.all_service_accounts) <= 0:
+            self.result_template(
+                check_id=check_id,
+                result=False,
+                reason="There is no gcp iam service account created",
+                resource_list=[],
+                description=description
+            )
+        else:
+            resource_list = []
+            owner_list = []
+            if len(self.policies) > 0:
+                for p in self.policies:
+                    if p['role'] == 'roles/owner' and 'user:' in str(p['members']):
+                        for member in p['members']:
+                            if 'user:' in member:
+                                owner_list.append(member)
+            if len(owner_list) > 1:
+                for mem in owner_list:
+                    resource_list.append(mem)
+
+            if len(resource_list) > 0:
+                result = True
+                reason = "Gcp project has more than one owner user"
+            else:
+                result = False
+                reason = "There is only one project owner"
+            return self.result_template(check_id, result, reason, resource_list, description)
+
+    # this method service account has project editor policy role
+    def check_3_5_service_account_has_editor_role(self):
+        check_id = 3.5
+        description = "Check for gcp service account has project editor policy role"
+        if len(self.all_service_accounts) <= 0:
+            self.result_template(
+                check_id=check_id,
+                result=False,
+                reason="There is no gcp iam service account created",
+                resource_list=[],
+                description=description
+            )
+        else:
+            resource_list = []
+            for sacc in self.all_service_accounts:
+                if len(self.policies) > 0:
+                    for p in self.policies:
+                        if sacc['name'] in p['members'] and p['role'] == 'roles/editor':
+                            resource_list.append(sacc['name'])
+
+            if len(resource_list) > 0:
+                result = True
+                reason = "Gcp service account has project editor policy role"
+            else:
+                result = False
+                reason = "There is no service account with project editor policy role"
+            return self.result_template(check_id, result, reason, resource_list, description)
+
+    # this method check gcp project has more than one editor user
+    def check_3_6_more_than_one_editor(self):
+        check_id = 3.6
+        description = "Check for gcp project has more than one editor user"
+        if len(self.all_service_accounts) <= 0:
+            self.result_template(
+                check_id=check_id,
+                result=False,
+                reason="There is no gcp iam service account created",
+                resource_list=[],
+                description=description
+            )
+        else:
+            resource_list = []
+            owner_list = []
+            if len(self.policies) > 0:
+                for p in self.policies:
+                    if p['role'] == 'roles/editor' and 'user:' in str(p['members']):
+                        for member in p['members']:
+                            if 'user:' in member:
+                                owner_list.append(member)
+            if len(owner_list) > 1:
+                for mem in owner_list:
+                    resource_list.append(mem)
+
+            if len(resource_list) > 0:
+                result = True
+                reason = "Gcp project has more than one editor user"
+            else:
+                result = False
+                reason = "There is only one project editor or there is no editor user"
+            return self.result_template(check_id, result, reason, resource_list, description)
+
+    # this method check gcp project viewer user
+    def check_3_7_there_is_project_viewer(self):
+        check_id = 3.7
+        description = "Check for gcp project has viewer user"
+        if len(self.all_service_accounts) <= 0:
+            self.result_template(
+                check_id=check_id,
+                result=False,
+                reason="There is no gcp iam service account created",
+                resource_list=[],
+                description=description
+            )
+        else:
+            resource_list = []
+            if len(self.policies) > 0:
+                for p in self.policies:
+                    if p['role'] == 'roles/viewer' and 'user:' in str(p['members']):
+                        for member in p['members']:
+                            if 'user:' in member:
+                                resource_list.append(member)
+
+            if len(resource_list) > 0:
+                result = True
+                reason = "Gcp project has viewer user"
+            else:
+                result = False
+                reason = "There is no project viewer user"
+            return self.result_template(check_id, result, reason, resource_list, description)
 
     # --- supporting methods ---
     # this method chek gcp iam service account has one or more keys
@@ -115,6 +263,7 @@ class IamResource:
         credentials = service_account.Credentials.from_service_account_file(service_account_file)
         # building gcp compute client using gcp compute v1 api
         self.iam_client = discovery.build('iam', 'v1', credentials=credentials)
+        self.resource_manager_client = discovery.build("cloudresourcemanager", "v1", credentials=credentials)
         self.project = project_id
 
     # this method returns information of all compute engine of all zones
@@ -125,6 +274,11 @@ class IamResource:
         for item in response['accounts']:
             service_accounts_info.append(item)
         return service_accounts_info
+
+    def all_policies(self):
+        policy = self.resource_manager_client.projects().getIamPolicy(resource=PROJECT_ID).execute()
+        return policy['bindings']
+
 
 
 class ExecuteCheckIam:
@@ -140,13 +294,19 @@ class ExecuteCheckIam:
         # getting resources for performing check
         resource_obj = IamResource(service_account_file=self.servive_account_file_path, project_id=self.project_id)
         all_service_accounts = resource_obj.all_service_accounts()
+        policies = resource_obj.all_policies()
         iam_client = resource_obj.iam_client
 
         # initiate Checks class
-        check_obj = IamChecks(iam_client=iam_client, all_service_accounts=all_service_accounts)
+        check_obj = IamChecks(iam_client=iam_client, all_service_accounts=all_service_accounts, policies=policies)
         all_check_result = [
             check_obj.check_3_1_service_account_user_managed_key(),
             check_obj.check_3_2_service_account_is_disabled(),
+            check_obj.check_3_3_service_account_has_owner_permission(),
+            check_obj.check_3_4_more_than_one_owner(),
+            check_obj.check_3_5_service_account_has_editor_role(),
+            check_obj.check_3_6_more_than_one_editor(),
+            check_obj.check_3_7_there_is_project_viewer(),
         ]
         check_obj.generate_csv(all_check_result)
 
